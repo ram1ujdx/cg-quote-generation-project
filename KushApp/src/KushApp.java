8cc5a13439bb1fb872c2e144721f6dfd88c01a0c
ramanujdas <ramanujds9@gmail.com>
2020-11-13 16:06:10 +0530

    Added curl braces on for loop

--- KushApp/src/KushApp.java
+++ KushApp/src/KushApp.java
@@ -3,9 +3,9 @@ public class KushApp {
 
 	public static void main(String[] args) {
 		
-		for(int i=0;i<5;i++)
+		for(int i=0;i<5;i++) {
 		System.out.println("Hello....... Kush here...");
-
+		}
 	}
 
 }
